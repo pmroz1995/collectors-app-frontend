02a60078295a0a6177ea96254e9a9620c3a29689
mrozik52 <pmroz1995@gmail.com>
2021-10-26 22:46:46 +0200

    Adding modal reference.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,23 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  public onOpenModal(coin: Coin, mode: string): void {
+    const button = document.createElement('button');
+    const container = document.getElementById('main-container');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'update') {
+      button.setAttribute('data-target', '#exampleModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
